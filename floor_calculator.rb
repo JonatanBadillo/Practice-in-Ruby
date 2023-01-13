1d7ccfe5cee0315627581bc54d98265ad812a98a
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-01-13 14:58:53 -0600

    Comments added

--- floor_calculator.rb
+++ floor_calculator.rb
@@ -1,12 +1,7 @@
+# Program that adds one position if there is '(' and subtract if thher is a ')' on the file text named "message.txt"
+
 text = File.read "message.txt"
-text = "(())"
-puts text
 
-counter = 0
-# where the for is going to stop
-stop = text.size-1
-
-for i in 0..stop
-    puts i
-end    
+result = text.split.collect{|n| n ==  '(' ? 1:-1 }.sum
+p result 
 
